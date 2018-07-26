37b1a6fbc6f76e6e2dd780de2f34ddacfb620697
Sander W. van der Laan <s.w.vanderlaan@gmail.com>
2018-07-27 00:14:05 +0200

    Bugs squashed

--- QTL_QC.R
+++ QTL_QC.R
@@ -431,7 +431,8 @@ if (!is.na(opt$projectdir) & !is.na(opt$resultfile) & !is.na(opt$outputdir) & !i
   ### Requires a bioconductor package: "qvalue"
   if(opt$resulttype == "NOM") {
   #RESULTS$Q = qvalue(RESULTS$Nominal_P)$qvalues # original code
-  RESULTS$Q = "Not calculated: throws an error when p-value is infinite or NA. NEED FIXING"
+  #RESULTS$Q = "Not calculated: throws an error when p-value is infinite or NA. NEED FIXING"
+  RESULTS$Q = ifelse(RESULTS$Nominal_P > 0, qvalue(RESULTS$Nominal_P)$qvalues, "NA")
   } else if(opt$resulttype == "PERM") {
    #RESULTS$Q = qvalue(RESULTS$Approx_Perm_P)$qvalues # original code
    RESULTS$Q = ifelse(RESULTS$Approx_Perm_P > 0, qvalue(RESULTS$Approx_Perm_P)$qvalues, "NA")
@@ -439,7 +440,7 @@ if (!is.na(opt$projectdir) & !is.na(opt$resultfile) & !is.na(opt$outputdir) & !i
    cat ("\n\n*** ERROR *** Something is rotten in the City of Gotham; most likely a typo. Double back, please.\n\n",
         file=stderr()) # print error messages to stder
   }
-  # RESULTS$Q = "Currently not calculated due to an issue with the qvalue() package."
+
   #--------------------------------------------------------------------------
   #### ADD IN THE ANNOTATIONS ###
   cat("\nApplying annotations.\n")
