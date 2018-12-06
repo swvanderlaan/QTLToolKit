3ee6a003cd2c6e9b1f933b4720052b25892c3d9b
Lennart Landsmeer <lennart@landsmeer.email>
2018-12-06 03:34:36 +0100

    pipeline: dry run by default

--- LENNART/pipeline/run-pipeline.sh
+++ LENNART/pipeline/run-pipeline.sh
@@ -1,37 +1,68 @@
 #!/usr/bin/bash
-set -xeu
+set -eu
 
 source config.sh
 
-mkdir -p ${CONFIG_JOBDIR}/besd
-cp config.sh $CONFIG_JOBDIR/config.sh
+setup () {
+    mkdir -p ${CONFIG_JOBDIR}/besd
+    cp config.sh $CONFIG_JOBDIR/config.sh
+    cat $REGIONSFILE | awk \
+        -v genetic="$GENETIC_ROOT" \
+        -v pre="$GENETIC_PRE" \
+        -v eqtl="$EQTL_ROOT" \
+        -v jobdir="$CONFIG_JOBDIR" \
+        -v post="$EQTL_POST" \
+        '{
+            chr = $3;
+            variant = $1;
+            gen = genetic "/" pre chr;
+            eqtl_file = eqtl "/" variant post "/ctmm_QC_qtlperm_" variant "_excl_EXCL_DEFAULT.txt.gz";
+            besd_output = jobdir "/besd/" variant
+            print variant, gen, eqtl_file, besd_output
+        }' > ${CONFIG_JOBDIR}/make-besd.param
+    NLINES=$(wc -l $REGIONSFILE | awk '$0=$1')
+    J=-$CONFIG_JOBNAME
+}
 
-cat $REGIONSFILE | awk \
-    -v genetic="$GENETIC_ROOT" \
-    -v pre="$GENETIC_PRE" \
-    -v eqtl="$EQTL_ROOT" \
-    -v jobdir="$CONFIG_JOBDIR" \
-    -v post="$EQTL_POST" \
-    '{
-        chr = $3;
-        variant = $1;
-        gen = genetic "/" pre chr;
-        eqtl_file = eqtl "/" variant post "/ctmm_QC_qtlperm_" variant "_excl_EXCL_DEFAULT.txt.gz";
-        besd_output = jobdir "/besd/" variant
-        print variant, gen, eqtl_file, besd_output
-    }' > ${CONFIG_JOBDIR}/make-besd.param
+DRY=true
+if [ $# -gt 0 ]; then
+    if [ "$1" == "--no-dry-run" ]; then
+        DRY=false
+    fi
+fi
 
-NLINES=$(wc -l job/make-besd.param | awk '$0=$1')
-J=-$CONFIG_JOBNAME
+sub () {
+    if $DRY; then
+        echo qsub "$@"
+    else
+        qsub $(echo "$@" | sed 's/%//g')
+    fi
+}
 
-qsub                          -N make-besd$J   -t 1-$NLINES -cwd job-files/make-besd.job    $CONFIG_JOBDIR/config.sh
-qsub -hold_jid_ad make-besd$J -N run-smr$J     -t 1-$NLINES -cwd job-files/run-smr.job      $CONFIG_JOBDIR/config.sh
-qsub -hold_jid    make-besd$J -N combine-besd$J             -cwd job-files/combine-besd.job $CONFIG_JOBDIR/config.sh
-qsub -hold_jid    run-smr$J   -N combine-smr$J              -cwd job-files/combine-smr.job  $CONFIG_JOBDIR/config.sh
+[ -d "$CONFIG_JOBDIR" ] && echo "WARNING job directory $CONFIG_JOBDIR already exists"
 
-echo ' === OUTPUT FILES === '
+setup
+
+main () {
+    sub %            %            -N make-besd$J    -t 1-$NLINES -cwd job-files/make-besd.job    $CONFIG_JOBDIR/config.sh
+    sub -hold_jid_ad make-besd$J  -N run-smr$J      -t 1-$NLINES -cwd job-files/run-smr.job      $CONFIG_JOBDIR/config.sh
+    sub -hold_jid    make-besd$J  -N combine-besd$J %  %         -cwd job-files/combine-besd.job $CONFIG_JOBDIR/config.sh
+    sub -hold_jid    run-smr$J    -N combine-smr$J  %  %         -cwd job-files/combine-smr.job  $CONFIG_JOBDIR/config.sh
+}
+
+if $DRY; then
+    echo 'this is dry run, no actual jobs are submitted, use --no-dry-run to qsub'
+    echo ' === QSUB ARGUMENTS ==='
+    main | column -t
+else
+    setup
+    main
+fi
+
+echo ' === OUTPUT FILES ==='
 echo ${CONFIG_JOBDIR}/smr.out
 echo ${CONFIG_JOBDIR}/besd/dense
-echo ' === OUTPUT FILES === '
+
+echo ' === QSTAT ==='
 
 qstat
